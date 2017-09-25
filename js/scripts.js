bc87c1027e3f0241fac8c33e8b40c683936cc0dc
Chris Turina and Kimberly Bordon <chris@>
2017-09-25 10:35:56 -0700

    create js to get form values

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,19 @@ function Task(name) {
 $(document).ready(function() {
   $("form#todo-form").submit(function(event) {
     event.preventDefault();
+
+    var inputtedTask = $("input#input-task").val();
+
+    var newTask = new Task(inputtedTask);
+
+    $("ul#list-tasks").append("<li><span class='task'>" + newTask.taskOne + "</span></li>");
+
+    $(".task").last().click(function() {
+      $("#show-tasks").show();
+      $("#show-tasks h2").text(newTask.taskOne);
+      $(".task-one").text(newTask.taskOne);
+    });
+
+    $("input#input-task").val("");
   });
 });
