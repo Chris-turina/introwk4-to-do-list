3977df5c4b1c0a0f922aba38a9399ab63e5661de
Chris Turina and Kimberly Bordon <kimberly@>
2017-09-25 11:56:51 -0700

    try to remove task...fail

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //business logic
-function Task(name) {
+function Task(name, details) {
   this.taskOne = name;
+  this.taskDetails = details;
+
 }
 
 //user interface logic
@@ -9,17 +11,26 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputtedTask = $("input#input-task").val();
+    var inputtedDetails = $("textarea#task-details").val();
 
-    var newTask = new Task(inputtedTask);
+    var newTask = new Task(inputtedTask, inputtedDetails);
 
-    $("ul#list-tasks").append("<li><span class='task'>" + newTask.taskOne + "</span></li>");
+    $("ul#list-tasks").append("<li><span class='task'>" + newTask.taskOne + " </span><button type='button' class='remove'>remove</button></li>");
 
     $(".task").last().click(function() {
       $("#show-tasks").show();
       $("#show-tasks h2").text(newTask.taskOne);
       $(".task-one").text(newTask.taskOne);
+      $(".task-details").text(newTask.taskDetails);
     });
 
     $("input#input-task").val("");
+    $("textarea#task-details").val("");
+
+  $("#remove-task").click(function(){
+    $("#show-task .task-one").empty();
+    $("#show-tasks").hide();
+
+  });
   });
 });
